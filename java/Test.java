5e407c0251eb9be670a08eb5490686a35a024ed2
Jack Joe <lancereally@foxmail.com>
2020-12-14 16:21:55 +0800

    add:git_demoss,Test.java

---
+++ java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @author Joe.
+ * @Program: git_demoss.
+ * @ClassName: .
+ * @date 2020年12月14日 16:20:44.
+ * @Copyright By Joe.
+ * @Description: .
+ * @Version: V1.0
+ */
+
+public class Test {
+}
